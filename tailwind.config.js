f98dcbb9dc3cd6889a5d435808b8a63e84f6ad78
Roseuwizeye123 <marieroseuwizeyimana440@gmail.com>
2024-03-19 14:06:36 -0700

    this is good

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode:"class",
   content: ["./html/**.{html,js}"],
   theme: {
     extend: {
+      
     screens: {
     sm:"480px",
     md:"768px",
+    lg:"1024px",
+      xl:"1280px",
 
     }
+    
 
     },
+    
     fontFamily:{
       nunito:['Nunito', 'sans-serif']
     }
